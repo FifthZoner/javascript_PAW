3c9defedc8980dd5b80d37d835874ca01fdec8b6
Szymon Jabłoński <173625@stud.prz.edu.pl>
2024-10-02 11:58:17 +0200

    commit w środku zad 2

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 function zad1() {
   const ex1 = document.getElementById("ex1_content");
-  const ex2 = document.getElementById("ex1_button");
-  ex2.addEventListener("click", function(){
+  const eb1 = document.getElementById("ex1_button");
+  eb1.addEventListener("click", function () {
     ex1.innerHTML = "1, 2, 3, 4, 5, 6, 7, 8, 9";
   });
+}
+
+function zad2() {
+  const et2 = document.getElementById("ex2_text");
+  const ex2 = document.getElementById("ex2_content");
+  let content = et2.value;
+  if (content.length !== 9) {
+    ex2.innerHTML = "Długość numeru musi być równa 9";
+    //return;
+  } else {
+    for (let n in content) {
+      if (n < "0" || n > "9") {
+        ex2.innerHTML = "Numer nie może zawierać liter";
+        return;
+      }
+    }
+  }
+  ex2.innerHTML = content;
 }
