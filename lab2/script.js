6e913deec57d9decb4f1872e771e774e4536308b
FifthZoner <102874986+FifthZoner@users.noreply.github.com>
2024-10-24 09:47:01 +0200

    Cm 2_6
    - dodałem tez z powrotem wyświetlanie wszystkiego na raz

--- lab2/script.js
+++ lab2/script.js
@@ -14,23 +14,39 @@
         })
   })
 
-  cw1.addEventListener("click", function () {
+    cw1.addEventListener("click", function () {
+        answer.innerHTML = "Loading...";
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(array => {
+                answer.innerHTML = "";
+                array.forEach(n =>{
+                    answer.innerHTML += "<b class = 'userId'>Id użytkownika: " + JSON.stringify(n.userId) + "</b>    ";
+                    answer.innerHTML += "<b class = 'id'>Id: " + JSON.stringify(n.id) + "</b><br>";
+                    answer.innerHTML += "<b class = 'title'>Tytuł: " + JSON.stringify(n.title) + "</b><br>";
+                    answer.innerHTML += "<b class = 'body'>Treść: " + JSON.stringify(n.body) + "</b><br><br>";
+                })
+                console.log(answer.innerText);
+            })
+    })
+
+  cw2.addEventListener("click", function () {
       answer.innerHTML = "Loading...";
       const number = 1; // TODO: zrobić to może dynamicznie
       const address = "https://jsonplaceholder.typicode.com/posts/" + number.toString();
       fetch(address)
           .then(response => response.json())
           .then(array => {
-              console.log(array);
               answer.innerHTML = "";
               answer.innerHTML += "<b class = 'userId'>Id użytkownika: " + JSON.stringify(array.userId) + "</b>    ";
               answer.innerHTML += "<b class = 'id'>Id: " + JSON.stringify(array.id) + "</b><br>";
               answer.innerHTML += "<b class = 'title'>Tytuł: " + JSON.stringify(array.title) + "</b><br>";
               answer.innerHTML += "<b class = 'body'>Treść: " + JSON.stringify(array.body) + "</b><br><br>";
+              console.log(answer.innerText);
           })
   })
 
-  cw2.addEventListener("click", function () {
+  cw3.addEventListener("click", function () {
       answer.innerHTML = "Processing...";
       const post = {
           userId : 1,
@@ -48,14 +64,12 @@
           .then(res => res.json())
           .then(data => {
               answer.innerHTML = "Dodano nowy post o ID = " + data.id;
+              console.log(answer.innerText);
           })
           .catch(error => {
               answer.innerHTML = "Nie udało się dodać postu!";
+              console.log(answer.innerText);
           });
-  })
-
-  cw3.addEventListener("click", function () {
-    //TODO
   })
 
 })();
